dc742b041149eaca11252e7c02f4ea1ce9912b09
Ygg <gex_klo@mail.ru>
2025-02-22 12:06:23 +0300

    Superstar
    a secre

--- app/src/main/java/com/ygg/uni_androiddev_3/MainActivity.java
+++ app/src/main/java/com/ygg/uni_androiddev_3/MainActivity.java
@@ -1,6 +1,8 @@
 package com.ygg.uni_androiddev_3;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private void superstar(View v) {
+        Toast.makeText(this, R.string.main_superstar, Toast.LENGTH_SHORT).show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
