094ba1a05b049cd55095e8971664de909f159cf8
Ygg <gex_klo@mail.ru>
2025-02-28 13:53:54 +0300

    Add scores
    For now just dummy stuff, will come into play once the other 3 activities are done.
    All 3 scores are displayed on SecondActivity and are reset every time the 'name' is changed.

--- app/src/main/java/com/ygg/uni_androiddev_3/SecondActivity.java
+++ app/src/main/java/com/ygg/uni_androiddev_3/SecondActivity.java
@@ -6,15 +6,49 @@ import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+
 public class SecondActivity extends AppCompatActivity {
+
+    // 3 dummy scores; why 3? Because ! :)
+    private Integer larioCoins;
+    private String larioCar;
+    private ArrayList<String> larioPublishes = new ArrayList<>();
+
+    public SecondActivity() {
+        resetScores();
+    }
+
+    private void resetScores() {
+        larioCoins = 0;
+        larioCar = "None";
+        larioPublishes.clear();
+    }
+
+    // a little helper to make my goddamn towers less of a tower
+    private void changeViewsVisibility(View[] views, int visibility) {
+        for (View v : views) {
+            v.setVisibility(visibility);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        /* keep scores updated */
+        final TextView scoreStar = findViewById(R.id.second_score_star);
+        final TextView scoreCash = findViewById(R.id.second_score_cash);
+        final TextView scoreCar = findViewById(R.id.second_score_car);
+
+        scoreStar.setText(getString(R.string.second_score_star, larioPublishes.size()));
+        scoreCash.setText(getString(R.string.second_score_cash, larioCoins));
+        scoreCar.setText(getString(R.string.second_score_car, larioCar));
 
         final EditText whofield = findViewById(R.id.second_edit_who);
 
@@ -34,12 +68,18 @@ public class SecondActivity extends AppCompatActivity {
 
             }
 
-            // after input is finished - show buttons if there is any text, otherwise hide
-            // also sus out anybody who thinks they can be Lario.
+            // if input is empty - hide everything
+            // if not - show everything
+            // and possibly catch impersonators
             @Override
             public void afterTextChanged(Editable s) {
+                resetScores();
+
                 if (s.toString().equalsIgnoreCase("lario")) {
                     findViewById(R.id.second_text_imposter).setVisibility(View.VISIBLE);
+                    changeViewsVisibility(new View[] {starButton, cashButton, carButton,
+                                    scoreStar, scoreCash, scoreCar},
+                            View.GONE);
                     return;
                 }
                 else {
@@ -47,15 +87,16 @@ public class SecondActivity extends AppCompatActivity {
                 }
 
                 if (s.toString().isEmpty()) {
-                    starButton.setVisibility(View.GONE);
-                    cashButton.setVisibility(View.GONE);
-                    carButton.setVisibility(View.GONE);
+                    changeViewsVisibility(new View[] {starButton, cashButton, carButton,
+                            scoreStar, scoreCash, scoreCar},
+                            View.GONE);
                     return;
                 }
 
-                starButton.setVisibility(View.VISIBLE);
-                cashButton.setVisibility(View.VISIBLE);
-                carButton.setVisibility(View.VISIBLE);
+
+                changeViewsVisibility(new View[] {starButton, cashButton, carButton,
+                                scoreStar, scoreCash, scoreCar},
+                        View.VISIBLE);
             }
         });
     }
