0e036ef9f13e2d606220a498c23aa1bbca6e0548
Ygg <gex_klo@mail.ru>
2025-02-28 12:29:07 +0300

    Modify Lario
    Lario has been modified. Lario is now not a static image. Lario is much more now.

--- app/src/main/java/com/ygg/uni_androiddev_3/MainActivity.java
+++ app/src/main/java/com/ygg/uni_androiddev_3/MainActivity.java
@@ -1,7 +1,11 @@
 package com.ygg.uni_androiddev_3;
 
+import android.animation.Animator;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -12,7 +16,8 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-    private void superstar(View v) {
+    // super secret toast that you get by clicking on Lario image
+    public void superstar(View v) {
         Toast.makeText(this, R.string.main_superstar, Toast.LENGTH_SHORT).show();
     }
 
@@ -20,5 +25,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // animating Lario
+        // first: create the animation with specific attributes
+        RotateAnimation rotateAnim = new RotateAnimation(
+                0f, 360f,
+                0f, 0f);
+        // and have it interpolated
+        rotateAnim.setInterpolator(new LinearInterpolator());
+        // and have it go forever
+        rotateAnim.setRepeatCount(RotateAnimation.INFINITE);
+        // and make it take 2 seconds to do a full loop
+        rotateAnim.setDuration(2000);
+
+        // assign the animation to our image
+        final ImageView larioView = findViewById(R.id.main_img_lario);
+        larioView.setAnimation(rotateAnim);
     }
+
 }
