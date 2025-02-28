811e47de344cb6a87220ac2ba5499da3daa5b1d4
Ygg <gex_klo@mail.ru>
2025-02-28 12:59:21 +0300

    Lay out Second activity
    + update manifest to add the activity
    + some new strings

---
+++ app/src/main/java/com/ygg/uni_androiddev_3/SecondActivity.java
@@ -0,0 +1,16 @@
+package com.ygg.uni_androiddev_3;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+
+    }
+}
