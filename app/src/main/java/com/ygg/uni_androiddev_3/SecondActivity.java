991c5a37188bcdd8507a5410b902dd4952c51325
Ygg <gex_klo@mail.ru>
2025-02-28 13:14:13 +0300

    Make Second activity functional
    It can now be accessed from MainActivity. MainActivity finishes after the switch.

--- app/src/main/java/com/ygg/uni_androiddev_3/SecondActivity.java
+++ app/src/main/java/com/ygg/uni_androiddev_3/SecondActivity.java
@@ -1,6 +1,11 @@
 package com.ygg.uni_androiddev_3;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,6 +16,38 @@ public class SecondActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
+        final EditText whofield = findViewById(R.id.second_edit_who);
 
+        final Button starButton = findViewById(R.id.second_button_superstar);
+        final Button cashButton = findViewById(R.id.second_button_money);
+        final Button carButton = findViewById(R.id.second_button_car);
+
+        // do some funny stuff based on text editing field
+        whofield.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            // after input is finished - show buttons if there is any text, otherwise hide
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (s.toString().isEmpty()) {
+                    starButton.setVisibility(View.GONE);
+                    cashButton.setVisibility(View.GONE);
+                    carButton.setVisibility(View.GONE);
+                    return;
+                }
+
+                starButton.setVisibility(View.VISIBLE);
+                cashButton.setVisibility(View.VISIBLE);
+                carButton.setVisibility(View.VISIBLE);
+            }
+        });
     }
 }
