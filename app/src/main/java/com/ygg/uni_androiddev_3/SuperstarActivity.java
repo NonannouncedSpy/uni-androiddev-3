570045d7ec43a39b997dd074eabf52ab77f1a8f0
Ygg <gex_klo@mail.ru>
2025-03-07 13:28:36 +0300

    Clean up some unused imports

--- app/src/main/java/com/ygg/uni_androiddev_3/SuperstarActivity.java
+++ app/src/main/java/com/ygg/uni_androiddev_3/SuperstarActivity.java
@@ -3,12 +3,7 @@ package com.ygg.uni_androiddev_3;
 import android.os.Bundle;
 import android.util.Log;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
 import java.util.ArrayList;
 
 public class SuperstarActivity extends AppCompatActivity {
